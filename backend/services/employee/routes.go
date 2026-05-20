4eea12b8ec42d05a8ea875f130aac48be1c021b0
emilija-99 <emaristic53@gmail.com>
2026-05-20 19:05:56 +0200

    db: users, roles and permissions finished and polishing

--- backend/services/employee/routes.go
+++ backend/services/employee/routes.go
@@ -10,6 +10,6 @@ type Handler struct {
 	validator *utils.Validator
 }
 
-func NewHandler(store types.Employee, v *utils.Validator) *Handler {
-	return &Handler{store: store, validator: v}
-}
+// func NewHandler(store types.Employee, v *utils.Validator) *Handler {
+// 	return &Handler{store: store, validator: v}
+// }
